342e5f6174204c207371dea1f1766a249fbe2f4f
Guohan Lu <gulv@microsoft.com>
2017-09-03 08:02:31 +0000

    [baseimage]: Disable consistent network device naming
    consistent network device naming is enabled in debian 9 kernel by default

--- installer/x86_64/install.sh
+++ installer/x86_64/install.sh
@@ -544,6 +544,7 @@ menuentry '$demo_grub_entry' {
         insmod part_msdos
         insmod ext2
         linux   /$image_dir/boot/vmlinuz-4.9.0-3-amd64 root=$demo_dev rw $GRUB_CMDLINE_LINUX  \
+                net.ifnames=0 biosdevname=0 \
                 loop=$image_dir/$FILESYSTEM_SQUASHFS loopfstype=squashfs                       \
                 apparmor=1 security=apparmor varlog_size=$VAR_LOG_SIZE $ONIE_PLATFORM_EXTRA_CMDLINE_LINUX
         echo    'Loading $demo_volume_label $demo_type initial ramdisk ...'
