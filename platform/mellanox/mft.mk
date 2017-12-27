0d45dfeb9c84327fd61106e47697667f3c411f33
Marian Pritsak <marianp@mellanox.com>
2017-12-27 20:46:37 +0200

    [Mellanox]: Upgrade MFT package to 4.8.26 (#3)
    This version is compatible with Linux kernel 4.9
    
    Signed-off-by: marian-pritsak <marianp@mellanox.com>

--- platform/mellanox/mft.mk
+++ platform/mellanox/mft.mk
@@ -1,9 +1,9 @@
 # Mellanox SAI
 
-MFT = mft-4.5.0-31.amd64.deb
+MFT = mft-4.8.0-26.amd64.deb
 $(MFT)_SRC_PATH = $(PLATFORM_PATH)/mft
 $(MFT)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_MAKE_DEBS += $(MFT)
 
-KERNEL_MFT = kernel-mft-dkms_4.5.0-3.16.0-4-amd64_all.deb
+KERNEL_MFT = kernel-mft-dkms_4.8.0-4.9.0-3-amd64_all.deb
 $(eval $(call add_derived_package,$(MFT),$(KERNEL_MFT)))
